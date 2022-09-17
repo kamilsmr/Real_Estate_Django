84d15a544e8af2a012269c620a6989705609d019
kamilsmr <kamils6035@gmail.com>
2022-09-17 18:57:57 +0300

    listings

---
+++ listings/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+# /listings/23
+urlpatterns = [
+    path('', views.index, name='listings'),
+    path('<int:lizting_id>', views.listing, name='listing'),
+    path('search', views.search, name='search'),
+
+
+]
