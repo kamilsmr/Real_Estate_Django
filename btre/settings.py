602b1cc3d15701dfc43f80f8f9a2a7713125583b
kamilsmr <kamils6035@gmail.com>
2022-09-17 23:56:23 +0300

    database

--- btre/settings.py
+++ btre/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '10121986Aa.',
+        'HOST': 'localhost'
+
     }
 }
 
