a303c7aecf3cd14c8b13ccbcc675419917fed3f4
kamilsmr <kamils6035@gmail.com>
2022-09-24 20:57:05 +0300

    MEDIA

--- btre/settings.py
+++ btre/settings.py
@@ -129,6 +129,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "btre/static"
 ]
 
+
+#media folder settings
+
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
