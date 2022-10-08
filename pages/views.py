cc31706644defd66506f3d07e55e87a7d83bbcb1
kamilsmr <kamils6035@gmail.com>
2022-10-08 23:18:13 +0300

    homepage

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+
 def index(request):
+    listings = Listing.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listing': listings
+    }
     return render(request,'pages/index.html')
 
 def about(request):
