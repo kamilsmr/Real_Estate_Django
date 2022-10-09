c56d9bdbef2a17f28d6126db6dbaa9f40ac76f0d
kamilsmr <kamils6035@gmail.com>
2022-10-09 12:28:20 +0300

    homepage

--- pages/views.py
+++ pages/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
-    listings = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
         'listing': listings
     }
-    return render(request,'pages/index.html')
+    return render(request,'pages/index.html', context)
 
 def about(request):
+    #getall realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    #Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context={
+        'realtors' : realtors,
+        'mvp_realtors' : mvp_realtors 
+    }
     return render(request,'pages/about.html')
