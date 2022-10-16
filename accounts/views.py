565500e9dd5b9037762ca5a565735935d0dca0bc
kamilsmr <kamils6035@gmail.com>
2022-10-16 23:19:09 +0300

    register

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 def register(request):
-    return render(request, 'accounts/register.html')
-
+    if request.method == 'POST':
+        # print('SUBMITTED REG')
+        # return redirect('register')
+        return 
+    else:
+        return render(request, 'accounts/register.html')
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # print('SUBMITTED REG')
+        # return redirect('login')
+        return 
+    else:
+        return render(request, 'accounts/login.html')
+ 
 
 def logout(request):
     return render('index')
