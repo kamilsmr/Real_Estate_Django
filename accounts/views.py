03bb436cc776a0bab2a64552ccfbd50e6e2172c6
kamilsmr <kamils6035@gmail.com>
2022-10-17 00:10:35 +0300

    control

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
         # print('SUBMITTED REG')
         # return redirect('register')
-        return redirect('register')
+
+        # check if passwords match
+        if password == password2:
+
+            # chech username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    return
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 def login(request):
