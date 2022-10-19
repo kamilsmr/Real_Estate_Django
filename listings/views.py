612500a526647b878a3a8dde326dbfe45ced8a52
kamilsmr <kamils6035@gmail.com>
2022-10-19 21:56:00 +0300

    finish

--- listings/views.py
+++ listings/views.py
@@ -1,76 +1,70 @@
-from ast import keyword
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from listings.choices import price_choices, bedroom_choices, state_choices
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
 def index(request):
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-    paginator = Paginator(listings, 6)
-    page = request.Get.get('page')
-    paged_listings = paginator.get_page(page)
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
 
-    context = {
-        'listings': paged_listings
-        
-    }
-    return render(request, 'listings/listings.html', context)
+  context = {
+    'listings': paged_listings
+  }
+
+  return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    listing = get_object_or_404(Listing, pk=listing_id)
+  listing = get_object_or_404(Listing, pk=listing_id)
 
-    context = {
-        'listing': listing
-    }
+  context = {
+    'listing': listing
+  }
 
-    return render(request, 'listings/listing.html')
+  return render(request, 'listings/listing.html', context)
 
 def search(request):
-    queryset_list = Listing.objects.order_by('-list_date')
+  queryset_list = Listing.objects.order_by('-list_date')
 
-    # Keywords
+  # Keywords
+  if 'keywords' in request.GET:
+    keywords = request.GET['keywords']
+    if keywords:
+      queryset_list = queryset_list.filter(description__icontains=keywords)
 
-    if 'keywords' in request.Get:
-       keywords = request.Get['keywords']
-       if keywords:
-        queryset_list =  queryset_list.filter(description__icontains=keywords)
+  # City
+  if 'city' in request.GET:
+    city = request.GET['city']
+    if city:
+      queryset_list = queryset_list.filter(city__iexact=city)
 
-    # City 
+  # State
+  if 'state' in request.GET:
+    state = request.GET['state']
+    if state:
+      queryset_list = queryset_list.filter(state__iexact=state)
 
-    if 'city' in request.Get:
-        city = request.Get['city']
-        if city:
-            queryset_list = queryset_list.filter(city__iexact=city)
+  # Bedrooms
+  if 'bedrooms' in request.GET:
+    bedrooms = request.GET['bedrooms']
+    if bedrooms:
+      queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
 
-    # State 
+  # Price
+  if 'price' in request.GET:
+    price = request.GET['price']
+    if price:
+      queryset_list = queryset_list.filter(price__lte=price)
 
-    if 'state' in request.Get:
-        state = request.Get['state']
-        if state:
-            queryset_list = queryset_list.filter(state__iexact=state)
-    
-    # Bedrooms
+  context = {
+    'state_choices': state_choices,
+    'bedroom_choices': bedroom_choices,
+    'price_choices': price_choices,
+    'listings': queryset_list,
+    'values': request.GET
+  }
 
-    if 'bedrooms' in request.Get:
-        bedrooms = request.Get['bedrooms']
-        if bedrooms:
-            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
-
-    # Price
-
-    if 'price' in request.Get:
-        price = request.Get['price']
-        if price:
-            queryset_list = queryset_list.filter(price__lte=price)
-
-
-    context = {
-        'state_choices': state_choices,
-        'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices,
-        'listings': queryset_list,
-        'values' : request.Get
-    }
-    return render(request, 'listings/search.html', context)
+  return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -9,23 +9,25 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listing': listings,
+        'listings': listings,
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices,
-
+        'price_choices': price_choices
     }
-    return render(request,'pages/index.html', context)
+
+    return render(request, 'pages/index.html', context)
+
 
 def about(request):
-    #getall realtors
+    # Get all realtors
     realtors = Realtor.objects.order_by('-hire_date')
 
-    #Get MVP
+    # Get MVP
     mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
 
-    context={
-        'realtors' : realtors,
-        'mvp_realtors' : mvp_realtors 
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
     }
-    return render(request,'pages/about.html')
+
+    return render(request, 'pages/about.html', context)
